a3420103ccc3121fd42bbf53bb7c927bc71da627
Prague1992 <37120984+Prague1992@users.noreply.github.com>
2018-11-11 12:03:22 -0500

    test2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ms from 'pretty-ms'
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      time: 0,
+      isOn: false,
+      start: 0
+    }
+    this.startTimer = this.startTimer.bind(this)
+    this.stopTimer = this.stopTimer.bind(this)
+    this.resetTimer = this.resetTimer.bind(this)
+  }
+  startTimer() {
+    this.setState({
+      isOn: true,
+      time: this.state.time,
+      start: Date.now() - this.state.time
+    })
+    this.timer = setInterval(() => this.setState({
+      time: Date.now() - this.state.start
+    }), 1);
+  }
+  stopTimer() {
+    this.setState({isOn: false})
+    clearInterval(this.timer)
+  }
+  resetTimer() {
+    this.setState({time: 0, isOn: false})
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    let start = (this.state.time == 0) ?
+      <button onClick={this.startTimer}>start</button> :
+      null
+    let stop = (this.state.time == 0 || !this.state.isOn) ?
+      null :
+      <button onClick={this.stopTimer}>stop</button>
+    let resume = (this.state.time == 0 || this.state.isOn) ?
+      null :
+      <button onClick={this.startTimer}>resume</button>
+    let reset = (this.state.time == 0 || this.state.isOn) ?
+      null :
+      <button onClick={this.resetTimer}>reset</button>
+    return(
+      <div>
+        <h3>timer: {ms(this.state.time)}</h3>
+        {start}
+        {resume}
+        {stop}
+        {reset}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
